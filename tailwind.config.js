3614244b64977ac98181457993345332a6090d0c
kyaw <khirokygn@gmail.com>
2024-10-18 23:19:03 +0700

    start

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'text': '#0a0806',
+        'background': '#fcfaf9',
+        'primary': '#493628',
+        'secondary': '#eaaa7b',
+        'accent': '#f3904a',
+       },
+    },
+  },
+  plugins: [],
+}
+
